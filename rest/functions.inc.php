5357ad6ca22fcede358f6ec5a5f4781dbb23fd44
Torsten Rüter <torsten@rueter.net>
2013-05-09 19:52:32 +0200

    cleanup

--- rest/functions.inc.php
+++ rest/functions.inc.php
@@ -1,8 +1,54 @@
 <?php
 
 
-function prepareMysql (){
+function indexAction(){
+    
+	$db = new PDO('mysql:host='.$_ENV['DATABASE_SERVER'].';dbname='.$_ENV['DATABASE_NAME'].';charset=utf8', $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
 
+    $statement = $db->query('SELECT * FROM hotspots');
+	$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+    print_r(json_encode($results));    
+}
+
+function showAction ($id) {
+    
+	$db = new PDO('mysql:host='.$_ENV['DATABASE_SERVER'].';dbname='.$_ENV['DATABASE_NAME'].';charset=utf8', $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
+	
+    $statement = $db->query('SELECT * FROM hotspots WHERE ID='.$id);
+	$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+    print_r(json_encode($results));  
+
+}
+
+function createAction() {
+
+/* Buggy
+	$db = new PDO('mysql:host='.$_ENV['DATABASE_SERVER'].';dbname='.$_ENV['DATABASE_NAME'].';charset=utf8', $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
+
+	$stmt = $db->prepare("INSERT INTO hotspots(name,xOff,yOff) VALUES('a','b','c')");
+	$stmt->execute();#array(':field1' => $_GET['name'], ':field2' => $_GET['xOff'], ':field3' => $_GET['yOff']));
+	$affected_rows = $stmt->rowCount();
+	die("affected rows : ".$affected_rows);
+	*/
+	echo "Hotspots#create";
+}
+
+function updateAction($id) {
+    echo "Hotspots#update:".$id;
+}
+
+function deleteAction($id) {
+    echo "Hotspots#delete:".$id;
+}
+
+function rootAction() {
+    echo "Slim Server running";
+}
+
+function prepareMysql (){
+/*
 	$link = mysql_connect($_ENV['DATABASE_SERVER'], $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
 	if (!$link) {
 	    die('Verbindung zur Datenbank schlug fehl: ' . mysql_error());
@@ -13,6 +59,10 @@ function prepareMysql (){
 	if (!$db_selected) {
     	die ('Kann '.$ENV['DATABASE_NAME'].' nicht benutzen : ' . mysql_error());
 	}
+*/
+
+	
+
 
 	return $link;	
 }
--- rest/index.php
+++ rest/index.php
@@ -2,68 +2,19 @@
 require 'Slim/Slim.php';
 require 'functions.inc.php';
 require 'environment_variables.php';
+
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
 
-$databaseLink = prepareMysql();
-
-$result = mysql_query("SHOW TABLES LIKE 'hotspots'");
-$tableExists = mysql_num_rows($result) > 0;
-
-#header("Content-Type: application/json");
 
 ## Home
-$app->get('/', function () {
-    echo "Welcome to my Slim Server";
-});
+$app->get('/', 'rootAction');
 
-## Index
-$app->get('/hotspots', function () {
-    
-    $array = array();
-    $result = mysql_query("SELECT * FROM hotspots");
-        while($row = mysql_fetch_assoc($result)){
-    
-            $array[] = $row;
-    }
-    
-    print_r(json_encode($array));    
-});
+$app->get('/hotspots', 'indexAction');
+$app->get('/hotspots/:id', 'showAction');
+$app->post('/hotspots', 'createAction');
+$app->delete('/hotspots/:id', 'deleteAction');
 
-// POST route
-$app->post('/hotspots', function () {
-    
-    print_r(json_encode($_GET));
-});
-
-## Show
-$app->get('/hotspots/:id', function ($id) {
-    $array = array();
-    $result = mysql_query("SELECT * FROM hotspots WHERE ID=".$id);
-        while($row = mysql_fetch_assoc($result)){
-            $array[] = $row;
-    }
-
-    print_r(json_encode($array));
-});
-
-
-## Show
-$app->put('/hotspots/:id', function ($id) {
-    echo "Hotspots#update";
-    echo "<br>";
-    print_r(json_encode($_GET));
-
-});
-
-// DELETE route
-$app->delete('/hotspots/:id', function () {
-    echo "Hotspots#delete";
-    echo "<br>";
-    print_r(json_encode($_GET));
-
-});
 
 $app->run();
 $app->response()->status(200);
-mysql_close($databaseLink);
