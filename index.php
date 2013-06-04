5716946f1beca90f8c4c5a7fcc2986fbac1923d9
Jens Grewe <jens0081@hotmail.de>
2013-06-04 22:41:32 +0200

    changed some stuff - 'ship' moves and 'map' moves too

--- index.php
+++ index.php
@@ -4,10 +4,27 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
-     <?php require("partials/header.php"); ?>
+     <!--<?php require("partials/header.php"); ?> -->
+     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Interaktive Karte</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/skrollr-fixed-setup.css" rel="stylesheet" type="text/css">
+    <link href="css/skrollr-keyframes.css" rel="stylesheet" type="text/css" data-skrollr-stylesheet>
+    <link href="css/main_skrollr.less" rel="stylesheet/less" type="text/css" data-skrollr-stylesheet>    
+
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
-
+        <div id="offsetHelper" class="always-on-top">0</div>
+        <div id="map_bg_img"></div>
+        <div id="ship_bg_img"></div>
+        <!--
         <div id="restdummy">
             <div id="map-wrapper">
                 <img id="#map-image" src="img/map1.jpg">
@@ -26,10 +43,17 @@
                 </form>
             </div>
         </div>
+        -->
 
+        <script type="text/javascript" src="js/vendor/less-1.3.3.min.js"></script>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <script type="text/javascript" src="js/vendor/skrollr.stylesheets.js"></script>
+        <script type="text/javascript" src="js/vendor/skrollr.min.js"></script>
+        <script type="text/javascript" src="js/vendor/skrollr.menu.js"></script>
+        <script type="text/javascript" src="js/vendor/prefixfree.min.js"></script>
+        <script type="text/javascript" src="js/main_skrollr.js"></script>
+        <script type="text/javascript" src="js/main_manageHotspots.js"></script>
 
-        <script src="js/main.js"></script>
     </body>
 </html>
---
+++ js/main_skrollr.js
@@ -0,0 +1,42 @@
+// Executed on document.ready event
+$(function() {
+
+	$(document).scroll(function(){	
+		var doc = document.documentElement, body = document.body;
+		var top = (doc && doc.scrollTop  || body && body.scrollTop  || 0);
+		$('#offsetHelper').html(top);
+	});
+
+	// Used to center elements
+
+	var nav = $('#nav');
+	var x = -1*(nav.outerWidth() / 2);
+	nav.css("margin-left", x);
+
+	var circle = $('#instruction-circle');
+	var x = -1*(circle.outerWidth() / 2);
+	circle.css("margin-left", x);
+	
+});
+
+// initialize skrollr
+var s = skrollr.init({
+	edgeStrategy: 'set',
+	/*easing: {
+		WTF: Math.random,
+		inverted: function(p) {
+			return 1-p;
+		}
+	}*/
+}); 
+// & menu
+skrollr.menu.init(s, {
+    animate: true, //skrollr will smoothly animate to the new position using `animateTo`.
+    duration: 500, //How long the animation should take in ms.
+    easing: 'sqrt' //The easing function to use.
+});
+
+// used in skrollr.stylesheets.js to keep skrollr data attributes clean
+String.prototype.killWhiteSpace = function() {
+    return this.replace(/\s/g, '');
+};
