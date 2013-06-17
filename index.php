2c1c8f644b0dd549c50f4480dc0ed966faea3ddf
Torsten Rüter <torsten@rueter.net>
2013-06-17 20:34:01 +0200

    first split map with designer graphics

--- index.php
+++ index.php
@@ -29,26 +29,36 @@
     -->
 
         <div id="offsetHelper">0</div>
+        <div id="map-wrapper">
 
-        <div id="minimap">minimap</div>
+            <div id="navigation"></div>
+            <div id="minimap">
+                <div id="current-location"></div>
+            </div>
 
-        <div id="map-1" class="map-holder">
-            <div id="ship-1" class="ships"></div>
+            <div id="map-1" class="map">
+                <div id="ship-1" class="ships">
+                    <img src="img/ship.png" class="ship-big">
+                </div>
+            </div>
+            
+            <div id="map-2" class="map">
+                <div id="ship-2" class="ships">
+                    <img src="img/ship.png" class="ship-small">
+                </div>
+            </div>
+            
+
+            <div id="map-3" class="map">
+                <div id="ship-3" class="ships"></div>
+            </div>
         </div>
-        
-        <div id="split-1" class="split-holder"><h1>Split #1</h1></div>
 
-        <div id="map-2" class="map-holder">
-            <div id="ship-2" class="ships"></div>
-        </div>
         
-        <div id="split-2" class="split-holder"><h1>Split #2</h1></div>
 
-        <div id="map-3" class="map-holder">
-            <div id="ship-3" class="ships"></div>
-        </div>
-        
-        <div id="split-3" class="split-holder"><h1>Split #3</h1></div>
+        <div id="split-1" class="split"><h1>Split #1</h1></div>
+        <div id="split-2" class="split"><h1>Split #2</h1></div>
+        <div id="split-3" class="split"><h1>Split #3</h1></div>
 
 
         <h1 style="text-align:center; margin-top: 400px">Ende</h1>
