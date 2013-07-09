395409b349f57cd8cae9198da529f0ab4bed8eec
dawidbany <banachowski.d@gmail.com>
2013-07-09 16:25:45 +0200

    fehler (php)

--- css/story-keyframes.php
+++ css/story-keyframes.php
@@ -44,7 +44,7 @@ $end = offsetTo($currentStory+1);
     <?=($end-100);   ?> { bottom: 80%; }
 }
 
-<?
+<?php
 ## Story 2
 
 $currentStory = 2; 
@@ -53,7 +53,7 @@ $end = offsetTo($currentStory+1);
 ?>
 
 
-<?
+<?php
 ## Story 3
 
 $currentStory = 3; 
@@ -65,10 +65,10 @@ $end = offsetTo($currentStory+1);
 #construction-wrapper_3{ -skrollr-animation-name : construction-wrapper_3; }
 @-skrollr-keyframes construction-wrapper_3 {
     <?=($start+100); ?> { width: 1300px; height: 900px; margin-top: -450px; margin-left: -650px;}
-    <?=($end-100);   ?> { width: 650px; height: 450px;; margin-top: -225px; margin-left: -325px;}
+    <?=($end-100);   ?> { width: 650px; height: 450px; margin-top: -225px; margin-left: -325px;}
 }
 
-<?
+<?php
 ## Story 4
 
 $currentStory = 4; 
@@ -76,7 +76,7 @@ $start = offsetTo($currentStory);
 $end = offsetTo($currentStory+1);
 ?>
 
-<?
+<?php
 ## Story 5
 
 $currentStory = 5; 
@@ -84,7 +84,7 @@ $start = offsetTo($currentStory);
 $end = offsetTo($currentStory+1);
 ?>
 
-<?
+<?php
 ## Story 6
 
 $currentStory = 6; 
@@ -98,14 +98,14 @@ $end = offsetTo($currentStory+1);
     <?=($start+100); ?> { right: 0%;}
     <?=($end-100);   ?> { right: 30%;}
 }
-<?
+<?php
 ## Story 7
 
 $currentStory = 7; 
 $start = offsetTo($currentStory);
 $end = offsetTo($currentStory+1);
 ?>
-<?
+<?php
 ## Story 8
 
 $currentStory = 8; 
@@ -129,7 +129,7 @@ $end = offsetTo($currentStory+1);
     <?=($end-100);   ?> { right: 0%;}
 }
 
-<?
+<?php
 ## Story 9
 
 $currentStory = 9; 
@@ -137,7 +137,7 @@ $start = offsetTo($currentStory);
 $end = offsetTo($currentStory+1);
 ?>
 
-<?
+<?php
 ## Story 10
 
 $currentStory = 10; 
