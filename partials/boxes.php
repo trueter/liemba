6bd23d714cd363014cc9beb9861f57a8325764e1
Torsten Rüter <torsten@rueter.net>
2013-07-12 09:32:00 +0200

    menu icons

---
+++ partials/boxes.php
@@ -0,0 +1,38 @@
+
+<div id="categories" class="green-box">
+    <ul id="category-selection">
+        <li data-category="stops">Anlegestellen</li>
+        <li data-category="activities">Aktivitäten</li>
+        <li data-category="airports">Flughäfen</li>
+        <li data-category="histories">Geschichte</li>
+        <li data-category="transports">Verkehr</li>
+        <li data-category="sightseeings">Tourismus</li>
+        <li data-category="hotels">Hotels</li>
+    </ul>
+</div>
+
+<div id="navigation" class="green-box">
+    <h2>Navigation</h2>
+    <ul id="navigation-stops">
+        <li><a href="#Bujumbura" data-menu-top="0">Bujumbura</a></li>
+        <li><a href="#Rumonge" data-menu-top="1590">Rumonge</a></li>
+        <li><a href="#Kigoma" data-menu-top="3608">Kigoma</a></li>
+        <li><a href="#Kirando" data-menu-top="5108">Kirando</a></li>
+        <li><a href="#Sigunda" data-menu-top="6576">Sigunda</a></li>
+        <li><a href="#Halembe" data-menu-top="6576">Halembe</a></li>
+        <li><a href="#Mugambo" data-menu-top="8076">Mugambo</a></li>
+        <li><a href="#Kibwesa" data-menu-top="9612">Kibwesa</a></li>
+        <li><a href="#Kalya" data-menu-top="9612">Kalya</a></li>
+        <li><a href="#Ikola" data-menu-top="11148">Ikola</a></li>
+        <li><a href="#Karema" data-menu-top="11148">Karema</a></li>
+        <li><a href="#Kabwe" data-menu-top="12830">Kabwe</a></li>
+        <li><a href="#Kirando" data-menu-top="14398">Kirando</a></li>
+        <li><a href="#Kipili" data-menu-top="14398">Kipili</a></li>
+        <li><a href="#Ninde" data-menu-top="15934">Ninde</a></li>
+        <li><a href="#Msamba" data-menu-top="15934">Msamba</a></li>
+        <li><a href="#Wanpembe" data-menu-top="15934">Wanpembe</a></li>
+        <li><a href="#Kala" data-menu-top="17616">Kala</a></li>
+        <li><a href="#Kasanga" data-menu-top="19298">Kasanga</a></li>
+        <li><a href="#Mpulungu" data-menu-top="20834">Mpulungu</a></li>
+    </ul>
+</div>
