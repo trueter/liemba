2f67f3cc02b6c1af53460edf155efed03567e221
Torsten Rüter <torsten@rueter.net>
2013-10-02 21:21:06 +0200

    update wordpress version, fix 404 page, remove heading frm page

--- wp-content/themes/liemba/404.php
+++ wp-content/themes/liemba/404.php
@@ -1,22 +1,25 @@
 <?php get_header(); ?>
 
-	<!-- section -->
-	<section role="main">
-	
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div id="sidebar-and-content">
+		<?php get_sidebar(); ?>
+		<!-- section -->
+		<section role="main" id="content-wrapper" class="black-item">
 		
-			<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-			<h2>
-				<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-			</h2>
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			
-		</article>
-		<!-- /article -->
-		
-	</section>
-	<!-- /section -->
+				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
+				<h2>
+					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
+				</h2>
+				
+			</article>
+			<!-- /article -->
+			
+		</section>
+		<!-- /section -->
+	</div>
 	
-<?php get_sidebar(); ?>
+
 
 <?php get_footer(); ?>
--- wp-content/themes/liemba/page.php
+++ wp-content/themes/liemba/page.php
@@ -5,9 +5,7 @@
 
 	<!-- section -->
 	<section id="content-wrapper" class="black-item" role="main">
-	
-		<h1><?php the_title(); ?></h1>
-	
+		
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 	
 		<!-- article -->
